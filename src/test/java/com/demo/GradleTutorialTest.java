406e53b60b9a4bcdadb9d85adb3845dc08425ace
Tom89757 <2689980244@qq.com>
2023-08-11 11:25:44 +0800

    add test manually

---
+++ src/test/java/com/demo/GradleTutorialTest.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.junit.Test;
+
+/**
+ * @author Tom89757
+ */
+public class GradleTutorialTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        GradleTutorial.main(new String[]{});
+    }
+}
