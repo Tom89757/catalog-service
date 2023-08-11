21bdcdb611dc58c3b36e585bf13132d6d4137c2a
Tom89757 <2689980244@qq.com>
2023-08-11 10:55:26 +0800

    init gradle tutorial

---
+++ src/main/java/com/demo/GradleTutorial.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+/**
+ * @author Tom89757
+ */
+public class GradleTutorial {
+    public static void main(String[] args) {
+        System.out.println("Gradle 4tw!");
+    }
+}
