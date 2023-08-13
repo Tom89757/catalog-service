8a4826ec7c9ba4b1aa0e768dd2011b4b06a7252a
Tom89757 <2689980244@qq.com>
2023-08-14 00:42:24 +0800

    init with spring-boot2 and maven

---
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -0,0 +1,16 @@
+package com.polarbookshop.catalogservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Tom89757
+ */
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String getGreeting() {
+        return "Welcome to the book catalog!";
+    }
+}
