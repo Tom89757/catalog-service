17b49c27c79a7e46d7590f200705f99c1be8f997
Tom89757 <2689980244@qq.com>
2023-08-22 01:13:22 +0800

    modify data related classes

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -4,13 +4,16 @@ package com.polarbookshop.catalogservice.domain;
  * @author Tom89757
  */
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
+import java.time.Instant;
 
 public record Book(
 
@@ -37,6 +40,12 @@ public record Book(
         )
         Double price,
 
+        @CreatedDate
+        Instant createdDate,
+
+        @LastModifiedDate
+        Instant lastModifiedDate,
+
         @Version
         int version
 
@@ -45,7 +54,7 @@ public record Book(
             String isbn, String title, String author, Double price
     ) {
         return new Book(
-                null, isbn, title, author, price, 0
+                null, isbn, title, author, price, null, null, 0
         );
     }
 }
--- src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
@@ -1,14 +1,24 @@
 package com.polarbookshop.catalogservice.domain;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
 import java.util.Optional;
 
 /**
  * @author Tom89757
  */
-public interface BookRepository {
-    Iterable<Book> findAll();
+public interface BookRepository extends CrudRepository<Book, Long> {
+//    Iterable<Book> findAll();
+//    Optional<Book> findByIsbn(String isbn);
+//    boolean existsByIsbn(String isbn);
+//    Book save(Book book);
+//    void deleteByIsbn(String isbn);
     Optional<Book> findByIsbn(String isbn);
     boolean existsByIsbn(String isbn);
-    Book save(Book book);
+
+    @Modifying
+    @Query("delete from Book where isbn = :isbn")
     void deleteByIsbn(String isbn);
 }
